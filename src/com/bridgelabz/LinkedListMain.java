84ee4ce1d38d9f0b8c3eff5aa8235ac9f7a971e1
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-27 19:49:34 +0530

    Added branch uc1 with push method

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -7,7 +7,17 @@ public class LinkedListMain {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Data Structure and Algorithm program ");
+        LinkedList l1 = new LinkedList();
 
+        int x;
+        System.out.println("How many element you required in linkedList ");
+        x=sc.nextInt();
+        for(   int i=1;i<=x;i++){
+            int y;
+            System.out.println("enter the key value of node"+ i +"=");
+            y=sc.nextInt();
+            l1.push(y);
+        }
 
     }
 }
